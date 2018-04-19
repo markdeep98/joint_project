b4327e09dadc2c420c34b72ec7334029d41b8a7d
melnichenko1996 <djek678@gmail.com>
2018-04-19 16:22:27 +0300

    next commit

--- new-project/config/routes.rb
+++ new-project/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  get 'help'    => 	'static_pages#help'
+  get 'about'   => 	'static_pages#about'
+  get 'contact' =>	'static_pages#contact'
+
 end
---
+++ new-project/test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get "/"
+    assert_response :success
+  end
+
+  test "should get help" do
+    get help_url
+    assert_response :success
+
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get about_contact
+    assert_response :success
+  end
+
+end
