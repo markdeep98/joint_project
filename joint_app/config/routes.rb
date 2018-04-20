c01507c4453451114244d54b10cad5f2b07027a4
markdeep98 <dmitriy.gladky98@gmail.com>
2018-04-20 05:40:31 -0700

    Registration

--- joint_app/config/routes.rb
+++ joint_app/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
 	root 'static_pages#home'
  	get 'help' => 'static_pages#help'
  	get 'about' => 'static_pages#about'
  	get 'contact' => 'static_pages#contact'
  	get 'signup' => 'users#new'
-
+ 	resources :users
 end
--- joint_app/test/models/user_test.rb
+++ joint_app/test/models/user_test.rb
@@ -9,7 +9,7 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do	
   	assert @user.valid?
   end
-
+ 	
   test "name should be present" do
   	@user.name = "     "
   	assert_not @user.valid?
