d871ca512ddaf3b8065da1e6546272459666d83b
markdeep98 <dmitriy.gladky98@gmail.com>
2018-04-24 00:54:46 -0700

    REST functions (update)

--- joint_app/app/controllers/users_controller.rb
+++ joint_app/app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  
+  before_action :admin_user,     only: :destroy
+
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -52,9 +53,19 @@ class UsersController < ApplicationController
       redirect_to(current_user) unless current_user?(@user)
     end
 
+    def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted"
+      redirect_to users_url
+    end
+
   private 
   	def user_params
   		params.require(:user).permit(:name, :email, :password,
   							 :password_confirmation)
   	end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
