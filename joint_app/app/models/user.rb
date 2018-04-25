01ea8f1fb5434b23a6f0982e946a2e00c5970d45
markdeep98 <dmitriy.gladky98@gmail.com>
2018-04-25 02:47:24 -0700

    Posts feed

--- joint_app/app/models/user.rb
+++ joint_app/app/models/user.rb
@@ -40,9 +40,12 @@ class User < ApplicationRecord
 	    update_attribute(:remember_digest, nil)
 	end
 
-	def feed
-		Micropost.where("user_id = ?", id)
-	end
+  	def feed
+    	following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+   		Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  	end
 
 	def follow(other_user)
 		active_relationships.create(followed_id: other_user.id)
